1b81c5717218f2423edf3aa8c3e30097c0c1d929
enrico25 <enrico.reitberger@googlemail.com>
2020-03-18 14:23:18 +0100

    -interface for ChatMessage was implemented -chat-window component was implemented is able to receive messages from Spring Java by Get-Request -Bootstrap was added in index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatWindowComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
