1d400cc8380d02028e1fcad27561ea2404a0ba4a
metav0id <volkerhauck@def-design.de>
2020-03-19 12:53:59 +0100

    Link from Landing-Page to contact-List works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactViewComponent} from "./contact-view/contact-view.component";
 import { ChatWindowComponent} from "./chat-window/chat-window.component";
+import { LandingpageComponent} from "./landingpage/landingpage.component";
 import { ContactViewUpdateComponent} from "./contact-view-update/contact-view-update.component";
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/landingpage', pathMatch: 'full'},
+  { path: 'landingpage', component: LandingpageComponent},
   { path: 'contacts', component: ContactViewComponent},
   { path: 'chat/:id', component: ChatWindowComponent},
   { path: 'update/:id', component: ContactViewUpdateComponent},
