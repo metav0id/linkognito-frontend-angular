b1eea85c30dca0252fb531f5bf642f4b78df6f4a
maher.saidani <maher.saidani@web.de>
2020-03-24 20:44:07 +0100

    Qr-Scann success

--- src/app/services/qr-scanner.service.ts
+++ src/app/services/qr-scanner.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { NewConnectionDto } from '../interfaces/new-connection-dto';
 import {SuccessDto} from '../interfaces/success-dto';
+import {User} from '../interfaces/user';
+import {IdDto} from '../interfaces/id-dto';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +18,11 @@ export class QrScannerService {
   private urlService = 'https://linkogservices.herokuapp.com/user/createconnection';
   qrStream: string = null;
 
-
   constructor(private http: HttpClient) { }
+
+  addScannId(scannId: number){
+    return this.http.post('http://localhost:8080/user', scannId);
+  }
 
   sendNewConnectionToService(newConnectionDTO: NewConnectionDto) {
     this.qrStream = newConnectionDTO.code;
