0671f7efc26c10944a2288bda14cf7e17930357b
maher.saidani <maher.saidani@web.de>
2020-03-25 15:54:35 +0100

    Qr-Scann success seite direkt

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -3,6 +3,12 @@ import { QrScannerService } from '../services/qr-scanner.service';
 import { NewConnectionDto } from '../interfaces/new-connection-dto';
 import {LoginService} from '../services/login.service';
 import {log} from 'util';
+import {ContactViewService} from '../services/contact-view.service';
+import {ContactInterface} from '../interfaces/contact.interface';
+import {Router} from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-scanner',
@@ -14,35 +20,72 @@ export class ScannerComponent implements OnInit {
   currentDevice: MediaDeviceInfo = null;
   hasPermission: boolean;
   qrStream: string;
-  newConnection: NewConnectionDto = {"id":119 , "code":"string"};
+  newConnection: NewConnectionDto = {"id":0 , "code":"string"};
+  contactInterface: ContactInterface;
 
-  // constructor(private qrScannerService: QrScannerService) {
-  // }
-
-  constructor(private qrScannerService: QrScannerService, private loginService: LoginService) {
+  constructor(
+    private qrScannerService: QrScannerService,
+    private loginService: LoginService,
+    private contactViewService: ContactViewService,
+    private router: Router
+    ){
     this.newConnection.id = loginService.loggedUser.id;
   }
 
   ngOnInit(): void {
   }
 
+  initContact(scannId: number) {
+    this.contactInterface = {
+      "id": this.loginService.loggedUser.id,
+      addressId: scannId,
+      "name": "new contact",
+      qrcode: this.qrStream,
+      active: true,
+      nickname:"anonymous"
+    };
+  }
+
+  logContactInterface() {
+    console.log(`this.contactInterface.id = ${this.contactInterface.id}`);
+    console.log(`this.contactInterface.name = ${this.contactInterface.name}`);
+    console.log(`this.contactInterface.addressId = ${this.contactInterface.addressId}`);
+    console.log(`this.contactInterface.nickname = ${this.contactInterface.nickname}`);
+    console.log(`this.contactInterface.qrcode = ${this.contactInterface.qrcode}`);
+    console.log(`this.contactInterface.active = ${this.contactInterface.active}`);
+
+  }
+
   onCodeResult(resultString: string): void {
     this.qrStream = resultString;
+    this.checkScann();
   }
 
   onHasPermission(has: boolean): void {
     this.hasPermission = has;
   }
 
-  sendNewConnectionToService (){
+  checkScann(){
     if (this.qrStream){
+      this.sendNewConnectionToService();
+      this.router.navigate(['/successScann']);
+    }
+  }
+
+  sendNewConnectionToService (){
       this.newConnection.code = this.qrStream;
       this.qrScannerService.sendNewConnectionToService(this.newConnection).subscribe(
         (result) => {
           console.log(`result.addressId = ${result.addressId}`);
           console.log(result.addressId);
+          this.initContact(result.addressId);
+          this.contactViewService.updateContact(this.contactInterface).subscribe(
+            () => {
+              console.log('addressId transmitted');
+              this.logContactInterface();
+            }
+          );
         }
       );
     }
-  }
 }
